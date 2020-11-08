fddd72982d1c4e277749c48076b735b8679a7ab1
Johan Appelgren <johan.appelgren@farnorthentertainment.com>
2020-11-08 15:29:36 +0100

    First commit and basic functionality done.

---
+++ GameServer/Server/ServerHandle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GameServer
+{
+	class ServerHandle
+	{
+		public static void WelcomeReceived(int clientId, Packet packet)
+		{
+			var claimedId = packet.ReadInt();
+			var userName = packet.ReadString();
+
+			Console.WriteLine($"{Server.Clients[clientId].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {clientId}.");
+
+			if (clientId != claimedId)
+				Console.WriteLine($"Player \"{userName}\" (ID: {clientId} has assumed the wrong client ID ({claimedId})!");
+			
+			//Send player into game here
+		}
+	}
+}
